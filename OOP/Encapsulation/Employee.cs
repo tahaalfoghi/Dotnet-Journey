86c798c646728b417f6729cfadf8deaba6d4b7c3
Taha Alfoghi <102795632+tahaalfoghi@users.noreply.github.com>
2023-04-30 12:31:14 +0200

    Add files via upload

---
+++ OOP/Encapsulation/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace MyClasses.Models
+{
+	/* Encapsulation */
+	public class Employee
+	{
+		public static double TAX;
+		public string? _fname { get;  private set; }
+		public string? _lname { get; private set; }
+		public double _wage { get; private set; }
+		private int _loggedHours { get; set; }
+		
+		private Employee(string? fname, string? lname, double wage, int loggedHours)
+		{
+			_fname=fname;
+			_lname=lname;
+			_wage=wage;
+			_loggedHours=loggedHours;
+		}
+		/* Factory Method */  
+		public static Employee Create(string? fname,string? lname,double wage,int loggedHours)
+		{
+			return new Employee(fname,lname,wage,loggedHours);
+		}
+		
+		/* Setter Method For The [First name & Last name */
+		public void setName(string? fname,string? lname)
+		{
+			if (string.IsNullOrWhiteSpace(fname) ||string.IsNullOrWhiteSpace(lname))
+				throw new ArgumentException("Invalid Input");
+			_fname=fname;
+			_lname=lname;
+		}
+		public double wage
+		{
+			get { return _wage; }
+		}
+		public void setWage(Object obj,double amount)
+		{
+			if (obj is not Manager)
+			{
+				throw new ArithmeticException("Invalid Operation");
+			}
+			else
+			{
+				_wage=amount;
+			}
+        }
+		public int loggedHours
+		{
+			get { return _loggedHours; }
+			set { _loggedHours=value; }
+		}
+		private double netSalary() => _wage* _loggedHours - (_wage* _loggedHours * TAX);
+
+
+		public override string ToString()
+		{
+			return $"[**Employee**]\nFirst Name:{_fname}\nLast Name: {_lname}\nWage: {_wage}" +
+				$"\nLoggedHours: {_loggedHours}\nNet Salary: {netSalary()}";
+		}
+		
+	}
+}
---
+++ OOP/Encapsulation/Manager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace MyClasses.Models
+{
+	public class Manager
+	{
+		public string? fname { get; private set; }
+		public string? lname { get; private set; }
+		private int _id { get; set; }
+		private double _salary { get; set; }
+		public double bonus { get; private set; }
+
+		private Manager(string? fname, string? lname, int id, double salary, double bonus)
+		{
+			this.fname=fname;
+			this.lname=lname;
+			_id=id;
+			this.salary=salary;
+			this.bonus=bonus;
+		}
+		public static Manager Create(string? fname,string? lname, int id,double salary,double bonus)
+		{
+			return new Manager(fname,lname,id,salary,bonus);
+		}
+		public void setName(string? fname,string? lname)
+		{
+			if (string.IsNullOrWhiteSpace(fname) || string.IsNullOrWhiteSpace(lname))
+			{
+				throw new ArgumentException("Invalid Input");		
+			}
+			else
+			{
+				this.fname=fname;
+				this.lname=lname;
+			}
+		}
+		public int id
+		{
+			get { return _id; }
+			set { _id=value; }
+		}
+		public double salary
+		{
+			get { return _salary+bonus; }
+			set { _salary=value; }
+		}
+		public void setBonus(double b) => bonus=b;
+		public double getBonus() =>bonus;
+
+		public override string? ToString()
+		{
+			return $"[**Manager**]\nFirst Name:{fname}\nLast Name:{lname}\nId:{_id}\nSalary:{salary}\n";
+		}
+		
+	}
+}
